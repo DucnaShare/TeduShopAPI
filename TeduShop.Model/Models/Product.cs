276f1515593c9f35b407b9abe02a14f2dd371c00
cskv.hocweb@gmail.com <24867193@vohoi>
2018-12-11 01:19:42 +0700

    fix error

--- TeduShop.Model/Models/Product.cs
+++ TeduShop.Model/Models/Product.cs
@@ -11,30 +11,22 @@ namespace TeduShop.Model.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { set; get; }
 
-        [Required]
-        [MaxLength(256)]
-        public string Name { set; get; }
+        [Required] [MaxLength(256)] public string Name { set; get; }
 
-        [Required]
-        [MaxLength(256)]
-        public string Alias { set; get; }
+        [Required] [MaxLength(256)] public string Alias { set; get; }
 
-        [Required]
-        public int CategoryID { set; get; }
+        [Required] public int CategoryID { set; get; }
 
-        [MaxLength(256)]
-        public string Image { set; get; }
+        [MaxLength(256)] public string Image { set; get; }
 
-        [Column(TypeName = "xml")]
-        public string MoreImages { set; get; }
+        [Column(TypeName = "xml")] public string MoreImages { set; get; }
 
         public decimal Price { set; get; }
 
         public decimal? PromotionPrice { set; get; }
         public int? Warranty { set; get; }
 
-        [MaxLength(500)]
-        public string Description { set; get; }
+        [MaxLength(500)] public string Description { set; get; }
 
         public string Content { set; get; }
 
@@ -42,6 +34,6 @@ namespace TeduShop.Model.Models
         public bool? HotFlag { set; get; }
         public int? ViewCount { set; get; }
 
-        [ForeignKey("CategoryID")]
-        public virtual ProductCategory ProductCategory { set; get; }
+        [ForeignKey("CategoryID")] public virtual ProductCategory ProductCategory { set; get; }
     }
+}
